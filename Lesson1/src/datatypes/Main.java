d95ea7696539a1480630672d73359fd9e5fdb84a
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-14 15:57:07 +0300

    DataTypes

---
+++ Lesson1/src/datatypes/Main.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		byte sayi = 31;
+		sayi = 127;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = true;
+	}
+
+}
