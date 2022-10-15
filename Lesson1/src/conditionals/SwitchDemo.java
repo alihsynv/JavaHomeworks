71635cb3008fc8d77be62ce6ad57e2c21a8c7616
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-15 15:22:53 +0300

    Switch demo

---
+++ Lesson1/src/conditionals/SwitchDemo.java
@@ -0,0 +1,36 @@
+package conditionals;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		char grade = 'F';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Mukemmel : Gectiniz");
+			break;
+
+		case 'B':
+			System.out.println("Cok guzel : Gectiniz");
+			break;
+
+		case 'C':
+			System.out.println("Iyi : Gectiniz");
+			break;
+
+		case 'D':
+			System.out.println("Fena degil : Gectiniz");
+			break;
+
+		case 'F':
+			System.out.println("Maalesef kaldiniz");
+			break;
+
+		default:
+			System.out.println("Gecersiz not girdiniz");
+
+		}
+
+	}
+
+}
