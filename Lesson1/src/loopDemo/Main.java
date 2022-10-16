7e47e38447be6781cbbb8e486b16c74945e5fb46
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-16 16:01:35 +0300

    Loop (While)

--- Lesson1/src/loopDemo/Main.java
+++ Lesson1/src/loopDemo/Main.java
@@ -1,14 +1,22 @@
 package loopDemo;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		// For
-		for (int i = 0; i < 10; i+=2) {
+		for (int i = 0; i < 10; i += 2) {
 			System.out.println(i);
 		}
 		System.out.println("Dongu bitti");
+
+		// While
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While Dongusu bitti");
+
 	}
 
 }
