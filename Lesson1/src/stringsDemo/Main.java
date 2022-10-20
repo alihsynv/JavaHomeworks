2611558ff02c6affb7a7ae80b4351564123f5ea3
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-20 11:12:30 +0400

    String Methods

--- Lesson1/src/stringsDemo/Main.java
+++ Lesson1/src/stringsDemo/Main.java
@@ -6,19 +6,33 @@ public class Main {
 		String mesaj = "Bugun hava cok guzel.";
 		System.out.println(mesaj);
 
-		System.out.println("Karakter sayisi : " + mesaj.length());
-		System.out.println("5. Karakter : " + mesaj.charAt(4));
-		System.out.println(mesaj.concat(" Yasasin!"));
-		System.out.println(mesaj.startsWith("B"));
-		System.out.println(mesaj.endsWith("."));
+//		System.out.println("Karakter sayisi : " + mesaj.length());
+//		System.out.println("5. Karakter : " + mesaj.charAt(4));
+//		System.out.println(mesaj.concat(" Yasasin!"));
+//		System.out.println(mesaj.startsWith("B"));
+//		System.out.println(mesaj.endsWith("."));
+//
+//		char[] karakterler = new char[5];
+//
+//		mesaj.getChars(0, 5, karakterler, 0);
+//		System.out.println(karakterler);
+//
+//		System.out.println(mesaj.indexOf("av"));
+//		System.out.println(mesaj.lastIndexOf("g"));
 
-		char[] karakterler = new char[5];
+		String yeniMesaj = mesaj.replace(' ', '_');
+		System.out.println(yeniMesaj);
 
-		mesaj.getChars(0, 5, karakterler, 0);
-		System.out.println(karakterler);
+		System.out.println(mesaj.substring(2, 8));
 
-		System.out.println(mesaj.indexOf("av"));
-		System.out.println(mesaj.lastIndexOf("g"));
+		for (String kelime : mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+
+		System.out.println(mesaj.trim());
 	}
 
 }
