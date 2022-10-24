66cbe92934f9a4cbd91840dfa9516f0d75e72ab5
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-24 14:34:41 +0400

    Variable Arguments

--- Lesson2/src/methods2/Main.java
+++ Lesson2/src/methods2/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		System.out.println(newMessage);
 		int number = gather(55, 7);
 		System.out.println(number);
+		int total = gather2(1, 2, 3, 4);
+		System.out.println(total);
 	}
 
 	public static void add() {
@@ -24,6 +26,14 @@ public class Main {
 
 	public static int gather(int number1, int number2) {
 		return number1 + number2;
+	}
+
+	public static int gather2(int... numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			total += number;
+		}
+		return total;
 	}
 
 	public static String city() {
