01c9e1d81328b7ca9382fbd131c9385327e603d8
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-28 22:51:50 +0400

    ReCapDemo (Classes)

---
+++ Lesson2/src/reCapDemo_Classes/FourTransactions.java
@@ -0,0 +1,19 @@
+package reCapDemo_Classes;
+
+public class FourTransactions {
+	public int Gather(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public int Subtract(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	public int Multiply(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public int Divide(int number1, int number2) {
+		return number1 / number2;
+	}
+}
---
+++ Lesson2/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,11 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourTransactions fourTransactions = new FourTransactions();
+		int result = fourTransactions.Gather(3, 4);
+		System.out.println(result);
+	}
+
+}
