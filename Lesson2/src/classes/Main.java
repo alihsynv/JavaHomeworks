561636f93f2d3df2a2c2837291f323d111ea47fb
Ali Hüseyn <ali.hsynov@gmail.com>
2022-10-28 17:36:21 +0400

    Value and reference types

--- Lesson2/src/classes/Main.java
+++ Lesson2/src/classes/Main.java
@@ -7,6 +7,21 @@ public class Main {
 		customerManager.Add();
 		customerManager.Remove();
 		customerManager.Update();
+
+		int number1 = 10;
+		int number2 = 20;
+		number2 = number1;
+		number1 = 30;
+		System.out.println(number2);
+
+		int[] numbers1 = new int[] { 1, 2, 3 };
+		int[] numbers2 = new int[] { 4, 5, 6 };
+		numbers2 = numbers1;
+		numbers2[0] = 10;
+		System.out.println(numbers2[0]);
+		for (int i : numbers2) {
+			System.out.println(i);
+		}
 	}
 
 }
